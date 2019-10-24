68d55643f36b5df0bc554cb8c28ce143e9775a4e
Sjarj <sebastien.jarjanette@icloud.com>
2019-10-24 16:01:50 +0200

    Test unitaire shallow

--- src/tests/todo-app.test.js
+++ src/tests/todo-app.test.js
@@ -4,8 +4,14 @@ import TodoApp from '../components/todo-app';
 import { shallow } from 'enzyme';
 
 describe('Test TodoApp fonctionnement', () => {
-  it('Render le composant todo sans erreur', () => {
+  it('Contient la chaine nouvelle tache', () => {
     const div = document.createElement('div');
     ReactDOM.render(<TodoApp />, div);
+    expect(div.innerHTML).toContain('Nouvelle tâche');
+  });
+
+  it('Possède 2 classes css row', () => {
+    const wrapper = shallow(<TodoApp />);
+    expect(wrapper.find('.row').length).toEqual(2);
   });
 });
