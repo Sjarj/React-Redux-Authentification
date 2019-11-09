4a09717a121a31200ca212a61206e91f2effbbb9
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-09 20:23:10 +0100

    Setup server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require("'morgan");
+const morgan = require("morgan");
 const expressServer = express();
+const router = require("./routes");
+const http = require("http");
+
+expressServer.use(morgan("combined"));
+expressServer.use(bodyParser.json({ type: "*/" }));
+
+const port = 3090;
+const server = http.createServer(expressServer);
+server.listen(port);
+console.log("le serveur écoute sur le port : ", port);
 // mongodb+srv://Sjarj:<password>@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority
