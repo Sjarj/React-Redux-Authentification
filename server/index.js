e8f868e351367a51a76b4ded10bfdb99bb143efd
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-09 20:00:38 +0100

    Add dependencies to setup  sever

--- server/index.js
+++ server/index.js
@@ -1,1 +1,5 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("'morgan");
+const expressServer = express();
 // mongodb+srv://Sjarj:<password>@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority
