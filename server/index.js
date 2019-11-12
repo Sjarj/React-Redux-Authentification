ca6d81d25f3648101c9db0fd6bd3092d9f354576
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-12 20:56:54 +0100

    Create a model user

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const morgan = require("morgan");
 const expressServer = express();
 const router = require("./routes");
 const http = require("http");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+mongoose.connection
+  .once("open", () => console.log("Connecté a Mlab"))
+  .on("error", error => console.error("Erreur de connexion", error));
 
 expressServer.use(morgan("combined"));
 expressServer.use(bodyParser.json({ type: "*/" }));
@@ -13,4 +22,3 @@ const server = http.createServer(expressServer);
 router(expressServer);
 server.listen(port);
 console.log("le serveur écoute sur le port : ", port);
-// mongodb+srv://Sjarj:<password>@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority
