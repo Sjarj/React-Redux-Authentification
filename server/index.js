c7c6558ef563a4412b118b0a4a79036828564a55
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-13 18:48:15 +0100

    Connexion to database

--- server/index.js
+++ server/index.js
@@ -4,6 +4,17 @@ const morgan = require("morgan");
 const expressServer = express();
 const router = require("./route");
 const http = require("http");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+mongoose.connection
+  .once("open", () => console.log("Connecté à Mlab"))
+  .on("error", () => {
+    console.log("Erreur de connexion à Mlab :", error);
+  });
 
 expressServer.use(morgan("combined"));
 expressServer.use(bodyParser.json({ type: "*/*" }));
