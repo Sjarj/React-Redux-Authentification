48e709b94dcf4a65f7686297ad641af9a9caa704
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-15 12:23:20 +0100

    Add strategy

--- server/route.js
+++ server/route.js
@@ -1,4 +1,6 @@
 const AuthentificationController = require("./controllers/authentification");
+const passport = require("passport");
+
 module.exports = function(expressServer) {
   expressServer.post("/signup", AuthentificationController.signup);
 };
---
+++ server/services/passport.js
@@ -0,0 +1,26 @@
+const passport = require("passport");
+const User = require("../models/user");
+const config = require("../../config");
+const jwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader("authorization"),
+  secretOrKey: config.secret
+};
+
+const jwtLogin = new jwtStrategy(jwtOptions, function(payload, done) {
+  const userId = payload.sub;
+  User.findById(userId, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);
