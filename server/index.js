b10e8e24fad324b993b1a9b5748d7ac5586d7bd7
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-20 16:52:09 +0100

    Add cors as middlewear

--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const expressServer = express();
-const router = require("./route");
-const http = require("http");
-const mongoose = require("mongoose");
+const router = require('./route');
+const http = require('http');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect(
-  "mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority",
+  'mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority',
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 mongoose.connection
-  .once("open", function() {
-    console.log("Connecté à Mlab");
+  .once('open', function() {
+    console.log('Connecté à Mlab');
   })
-  .on("error", function(error) {
-    console.log("Erreur de connexion à Mlab :", error);
+  .on('error', function(error) {
+    console.log('Erreur de connexion à Mlab :', error);
   });
 
-expressServer.use(morgan("combined"));
-expressServer.use(bodyParser.json({ type: "*/*" }));
+expressServer.use(morgan('combined'));
+expressServer.use(bodyParser.json({ type: '*/*' }));
+expressServer.use(cors());
 
 const port = 3090;
 const server = http.createServer(expressServer);
 router(expressServer);
 server.listen(port);
-console.log("Le serveur écoute sur le port :", port);
+console.log('Le serveur écoute sur le port :', port);
